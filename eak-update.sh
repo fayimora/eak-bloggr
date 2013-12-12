8328140fedd3477871601dfec1d980302e1910ab
Fayimora Femi-Balogun <fayi@fayimora.com>
2013-12-12 13:32:59 +0000

    Add install script

---
+++ eak-update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Ember app kit version update
+APPDIR="/Users/fayimora/Code/Javascript/Ember/eak-bloggr"
+PATCHDIR="/tmp/eakpatch"
+EAKDIR="/tmp/ember-app-kit-master"
+
+cd ${EAKDIR}
+git pull
+cd ${APPDIR}
+
+# first generate patches of important files
+FILES="bower.json package.json Gruntfile.js karma.conf.js .jshintsrc .bowerrc" # app/app.js app/adapters/application.js"
+
+echo "Patching application code with updates"
+mkdir -p ${PATCHDIR}
+
+for i in ${FILES}
+do
+  mkdir -p ${PATCHDIR}/$(dirname ${i})
+  diff -Nau ${APPDIR}/${i} ${EAKDIR}/${i} > ${PATCHDIR}/${i}
+  patch ${APPDIR}/${i} ${PATCHDIR}/${i}
+done
+
+echo "updating tasks directory"
+rm -rf ${APPDIR}/tasks/*
+cp -r ${EAKDIR}/tasks/* ${APPDIR}/tasks/
+
+cd ${APPDIR}
+echo "running npm update"
+npm update
+
+echo "running bower update"
+bower update
